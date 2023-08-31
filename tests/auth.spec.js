c5fce6af2829384c27bf5f129ceb20a6ffe88fbf
Artem <artyom.profis@gmail.com>
2023-08-31 11:59:05 -0400

    Sign in negative-password field empty

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -10,4 +10,14 @@ test.describe('Authentication & Authorization', () => {
 
         await expect(page.locator('.ant-avatar-square')).toBeVisible()
     })
+    test('Sign in negative-password field empty', async ({page}) => {
+        await page.goto('https://coding.pasv.us/user/login')
+
+        await page.locator('#normal_login_email').fill('artyom.profis@gmail.com')
+        await page.locator('#normal_login_password').fill('')
+        await page.locator('button[type="submit"]').isDisabled()
+
+        //await expect(page.locator('.ant-form-item-explain-error')).toHaveText('Required')
+    })
+
 })
