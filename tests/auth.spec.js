4a5043547eff95c1d7ae48e5955a115a9cddd99a
Artem <artyom.profis@gmail.com>
2023-09-01 11:47:10 -0400

    Sign in negative-invalid password

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -19,13 +19,25 @@ test.describe('Authentication & Authorization', () => {
 
         //await expect(page.locator('.ant-form-item-explain-error')).toHaveText('Required')
     })
-    test.only('Sign in negative-password field empty2', async ({page}) => {
+    test('Sign in negative-password field empty2', async ({page}) => {
         await page.goto('https://coding.pasv.us/user/login')
 
         await page.locator('#normal_login_email').fill('artyom.profis@gmail.com')
         await page.locator('#normal_login_password').fill('1')
         await page.keyboard.press('Backspace')
         await page.locator('.ant-form-item-explain-error').isVisible()
+        //await page.waitForTimeout(5000)
+
+        //await expect(page.locator('.ant-form-item-explain-error')).toHaveText('Required')
+    })
+    test.only('Sign in negative-invalid password', async ({page}) => {
+        await page.goto('https://coding.pasv.us/user/login')
+
+        await page.locator('#normal_login_email').fill('artyom.profis@gmail.com')
+        await page.locator('#normal_login_password').fill('pasv#2023')
+        await page.keyboard.press('Enter')
+        await page.locator('.ant-notification-notice-message').isVisible()
+        await expect(page.locator('.ant-notification-notice-message')).toHaveText('User login. Fail')
         //await page.waitForTimeout(5000)
 
         //await expect(page.locator('.ant-form-item-explain-error')).toHaveText('Required')
