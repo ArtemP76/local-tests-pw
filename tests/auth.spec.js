e69723f70267391379c78c6d75c1ac23e4b34b83
Artem <artyom.profis@gmail.com>
2023-09-11 14:49:50 -0400

    Install .env

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,11 +1,15 @@
 const { test, expect } = require('@playwright/test');
 
 test.describe('Authentication & Authorization', () => {
+    test.beforeEach(async ({page, browser}) => {
+    await page.goto('/user/login')
+    })
+
     test('Sign in with existing credentials', async ({page}) => {
         await page.goto('https://coding.pasv.us/user/login')
 
-        await page.locator('#normal_login_email').fill('artyom.profis@gmail.com')
-        await page.locator('#normal_login_password').fill('PASV#2023')
+        await page.locator('#normal_login_email').fill(process.env.EMAIL)
+        await page.locator('#normal_login_password').fill(process.env.PASSWORD)
         await page.locator('button[type="submit"]').click()
 
         await expect(page.locator('.ant-avatar-square')).toBeVisible()
